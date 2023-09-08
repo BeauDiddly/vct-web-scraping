5ec08351314a2accacc4b673146f14e267f65cdc
Ryan Luong <ryanluong83@gmail.com>
2023-09-08 15:42:41 -0700

    Done with the general code to get pick rates

--- lock-in-sao-paulo/stats/alpha/rounds_of_sixteen/get_agents_pick_rates.py
+++ lock-in-sao-paulo/stats/alpha/rounds_of_sixteen/get_agents_pick_rates.py
@@ -49,4 +49,24 @@ agents_pick_rates = []
 for pick_rate in pick_rates:
     agents_pick_rates.append(pick_rate.text.strip())
 
-print(agents_pick_rates)
+# print(agents_pick_rates)
+
+max_agents = len(agents_names)
+pick_rate_index = 0
+map_pick_rates = {}
+
+for i in range(0, max_rows):
+    map_name = maps_name[i]
+    map_pick_rates[map_name] = {}
+    for index, name in enumerate(agents_names):
+        pick_rate = agents_pick_rates[pick_rate_index + index] 
+        map_pick_rates[map_name][name] = pick_rate
+    pick_rate_index += max_agents
+    # map_name = maps_stats[index].text.strip()
+    # map_counts = maps_stats[index + 1].text.strip()
+    # atk_win_percentage = maps_stats[index + 2].text.strip()
+    # def_win_percentage = maps_stats[index + 3].text.strip()
+    # maps_stats_dict[map_name] = {"map_counts": map_counts, "atk_win": atk_win_percentage, "def_win": def_win_percentage}
+    # index += 4
+
+print(map_pick_rates)
