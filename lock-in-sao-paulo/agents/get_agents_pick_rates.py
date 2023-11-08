0cdb63dc6ae7d215661288a702bb21983c0f8c0a
Ryan Luong <ryanluong83@gmail.com>
2023-11-07 22:46:29 -0800

    Got the maps and agents pick rates

--- lock-in-sao-paulo/agents/get_agents_pick_rates.py
+++ lock-in-sao-paulo/agents/get_agents_pick_rates.py
@@ -4,19 +4,19 @@ import re
 import time
 # url = f"https://www.vlr.gg/event/agents/1188/champions-tour-2023-lock-in-s-o-paulo?exclude=16338.16339.16332.16334.16335.16336.16337"
 
-urls = {"all_stages": "https://www.vlr.gg/event/agents/1188/champions-tour-2023-lock-in-s-o-paulo",
-        "semi_finals": "https://www.vlr.gg/event/agents/1188/champions-tour-2023-lock-in-s-o-paulo?exclude=16339.16332.16333.16334.16335.16336.16337",
-        "grand_finals": "https://www.vlr.gg/event/agents/1188/champions-tour-2023-lock-in-s-o-paulo?exclude=16338.16332.16333.16334.16335.16336.16337",
-        "playoffs": "https://www.vlr.gg/event/agents/1188/champions-tour-2023-lock-in-s-o-paulo?exclude=16332.16333.16334.16335.16336.16337",
-        "alpha_round_of_sixteen": "https://www.vlr.gg/event/agents/1188/champions-tour-2023-lock-in-s-o-paulo?exclude=16338.16339.16333.16334.16335.16336.16337",
-        "alpha_quarterfinals": "https://www.vlr.gg/event/agents/1188/champions-tour-2023-lock-in-s-o-paulo?exclude=16338.16339.16332.16334.16335.16336.16337",
-        "alpha_semifinals": "https://www.vlr.gg/event/agents/1188/champions-tour-2023-lock-in-s-o-paulo?exclude=16338.16339.16332.16333.16335.16336.16337",
-        "alpha": "https://www.vlr.gg/event/agents/1188/champions-tour-2023-lock-in-s-o-paulo?exclude=16338.16339.16335.16336.16337",
-        "omega_round_of_sixteen": "https://www.vlr.gg/event/agents/1188/champions-tour-2023-lock-in-s-o-paulo?exclude=16338.16339.16332.16333.16334.16336.16337",
-        "omega_quarterfinals": "https://www.vlr.gg/event/agents/1188/champions-tour-2023-lock-in-s-o-paulo?exclude=16338.16339.16332.16333.16334.16335.16337",
-        "omega_semifinals": "https://www.vlr.gg/event/agents/1188/champions-tour-2023-lock-in-s-o-paulo?exclude=16338.16339.16332.16333.16334.16335.16336",
-        "omega": "https://www.vlr.gg/event/agents/1188/champions-tour-2023-lock-in-s-o-paulo?exclude=16338.16339.16332.16333.16334",
-        "bracket_stage": "https://www.vlr.gg/event/agents/1188/champions-tour-2023-lock-in-s-o-paulo?exclude=16338.16339"}
+# urls = {"all_stages": "https://www.vlr.gg/event/agents/1188/champions-tour-2023-lock-in-s-o-paulo",
+#         "semi_finals": "https://www.vlr.gg/event/agents/1188/champions-tour-2023-lock-in-s-o-paulo?exclude=16339.16332.16333.16334.16335.16336.16337",
+#         "grand_finals": "https://www.vlr.gg/event/agents/1188/champions-tour-2023-lock-in-s-o-paulo?exclude=16338.16332.16333.16334.16335.16336.16337",
+#         "playoffs": "https://www.vlr.gg/event/agents/1188/champions-tour-2023-lock-in-s-o-paulo?exclude=16332.16333.16334.16335.16336.16337",
+#         "alpha_round_of_sixteen": "https://www.vlr.gg/event/agents/1188/champions-tour-2023-lock-in-s-o-paulo?exclude=16338.16339.16333.16334.16335.16336.16337",
+#         "alpha_quarterfinals": "https://www.vlr.gg/event/agents/1188/champions-tour-2023-lock-in-s-o-paulo?exclude=16338.16339.16332.16334.16335.16336.16337",
+#         "alpha_semifinals": "https://www.vlr.gg/event/agents/1188/champions-tour-2023-lock-in-s-o-paulo?exclude=16338.16339.16332.16333.16335.16336.16337",
+#         "alpha": "https://www.vlr.gg/event/agents/1188/champions-tour-2023-lock-in-s-o-paulo?exclude=16338.16339.16335.16336.16337",
+#         "omega_round_of_sixteen": "https://www.vlr.gg/event/agents/1188/champions-tour-2023-lock-in-s-o-paulo?exclude=16338.16339.16332.16333.16334.16336.16337",
+#         "omega_quarterfinals": "https://www.vlr.gg/event/agents/1188/champions-tour-2023-lock-in-s-o-paulo?exclude=16338.16339.16332.16333.16334.16335.16337",
+#         "omega_semifinals": "https://www.vlr.gg/event/agents/1188/champions-tour-2023-lock-in-s-o-paulo?exclude=16338.16339.16332.16333.16334.16335.16336",
+#         "omega": "https://www.vlr.gg/event/agents/1188/champions-tour-2023-lock-in-s-o-paulo?exclude=16338.16339.16332.16333.16334",
+#         "bracket_stage": "https://www.vlr.gg/event/agents/1188/champions-tour-2023-lock-in-s-o-paulo?exclude=16338.16339"}
 
 url = "https://www.vlr.gg/vct-2023"
 page = requests.get(url)
@@ -63,13 +63,65 @@ for tournament, url in urls.items():
     all_ids = all_ids.strip(".").split(".")
     for stage_name, match_types in tournament_dict.items():
         for match_type, id in match_types.items():
-            excluded_ids = ".".join(included_id for included_id in all_ids if included_id != id)
+            excluded_ids = ".".join(exclude_id for exclude_id in all_ids if exclude_id != id)
             filter_url = f"{url}?exclude={excluded_ids}"
             tournament_dict[stage_name][match_type] = filter_url
+    tournament_dict["All"] = {}
+    tournament_dict["All"]["All"] = f"{url}?exclude"
     
     break
-print(stages_filter)
+pattern = r'/(\w+)\.png'
+pick_rates = {"maps_stats": {}, "agents_pick_rates": {}, "teams_pick_rates": {}}
 
+
+for tournament_name, stages in stages_filter.items():
+    for stage_name, match_types in stages.items():
+        for match_type_name, url in match_types.items():
+            page = requests.get(url)
+            soup = BeautifulSoup(page.content, "html.parser")
+            global_maps_table = soup.find("table", class_="wf-table mod-pr-global")
+            agent_pictures = global_maps_table.find_all("th", style=" vertical-align: middle; padding-top: 0; padding-bottom: 0; width: 65px;")
+            table_headers = []
+            global_table_titles = ["Map", "Total Played", "Attacker Side Win", "Defender Side Win"]
+
+
+
+            for th in agent_pictures:
+                src = th.find("img").get("src")
+                match = re.search(pattern, src)
+                agent = match.group(1)
+                global_table_titles.append(agent)
+            
+            table_stats_tr = global_maps_table.find_all("tr")[1:]
+
+            for tr in table_stats_tr:
+                all_tds = tr.find_all("td")
+                filtered_tds = [td for td in all_tds if isinstance(td, Tag)]
+                for index, td in enumerate(filtered_tds):
+                    print(td)
+                    td_class = td.get("class") or ""
+                    class_name = " ".join(td_class)
+                    if not class_name:
+                        map = td.text.strip().replace("\t", "")
+                        if not map:
+                            map = "All"
+                        else:
+                            logo, map = map.split("\n")
+                        maps_stats_dict = pick_rates["maps_stats"].setdefault(map, {})
+                        agents_pick_rates_dict = pick_rates["agents_pick_rates"].setdefault(map, {})
+                    elif class_name == "mod-right":
+                        stat = td.text.strip()
+                        title = global_table_titles[index]
+                        maps_stats_dict[title] = stat
+                    elif class_name == "mod-center":
+                        stat = td.text.strip()
+                        agent = global_table_titles[index]
+                        agents_pick_rates_dict[agent] = stat
+
+
+
+print(pick_rates["maps_stats"])
+print(pick_rates["agents_pick_rates"])
 
 
 
