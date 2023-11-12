057749bca75b011b9923b5242a4014428481a8a6
Ryan Luong <ryanluong83@gmail.com>
2023-11-11 22:26:42 -0800

    Need to get the players stats to the csv file next

--- lock-in-sao-paulo/stats/get_players_stats.py
+++ lock-in-sao-paulo/stats/get_players_stats.py
@@ -57,9 +57,9 @@ pattern = r'/(\w+)\.png'
 
 
 
-players_stats = {}
+all_agents = ["astra", "breach", "brimstone", "chamber", "cypher", "deadlock", "fade", "gekko", "harbor", "iso", "jett", "kayo",
+              "killjoy", "neon", "omen", "phoenix", "raze", "reyna", "sage", "skye", "sova", "viper", "yoru", "all"]
 
-players_with_one_agent_played = set()
 
 stats_titles = []
 
@@ -74,7 +74,7 @@ filtered_urls = {}
 
 players_stats = {}
 
-players_agents = {}
+global_players_agents = {}
 
 stats_titles = ["", "", "Rounds Played", "Rating", "Average Combat Score", "Kills:Deaths", "Kill, Assist, Trade, Survive %",
                 "Average Damage per Round", "Kills Per Round", "Assists Per Round", "First Kills Per Round", "First Deaths Per Round", 
@@ -130,62 +130,17 @@ for tournament_name, stages in filtered_urls.items():
         stage_dict = tournament_dict.setdefault(stage_name, {})
         for match_type_name, url in match_types.items():
             match_type_dict = stage_dict.setdefault(match_type_name, {})
-            page = requests.get(url)
-            soup = BeautifulSoup(page.content, "html.parser")
-
-            all_agents = set()
-            players_with_one_agent_played = set()
-            stats_trs = soup.find_all("tr")[1:]
-
-            for tr in stats_trs:
-                all_tds = tr.find_all("td")
-                filtered_tds = [td for td in all_tds if isinstance(td, Tag)]
-                for index, td in enumerate(filtered_tds):
-                    td_class = td.get("class") or ""
-                    class_name = " ".join(td_class)
-                    if class_name == "mod-player mod-a":
-                        player_info = td.find("div").find_all("div")
-                        player, team = player_info[0].text, player_info[1].text
-                        team_dict = match_type_dict.setdefault(team, {})
-                        player_dict = team_dict.setdefault(player, {})
-                    elif class_name == "mod-agents":
-                        imgs = td.find("div").find_all("img")
-                        agents = ""
-                        for img in imgs:
-                            src = img.get("src")
-                            match = re.search(pattern, src)
-                            agent = match.group(1)
-                            all_agents.add(agent)
-                            agents += f"{agent}, "
-                        agents = agents.strip(", ")
-                        agents_dict = player_dict.setdefault(agents, {})
-                        if len(imgs) == 1:
-                            players_with_one_agent_played.add(player)
-                    elif class_name == "mod-rnd" or class_name == "mod-cl" or class_name == "":
-                        stat = td.text.strip()
-                        stat_name = stats_titles[index]
-                        if stat == "":
-                            stat = "-1"
-                        agents_dict[stat_name] = stat
-                    elif class_name == "mod-color-sq mod-acs" or class_name ==  "mod-color-sq":
-                        stat = td.find("div").find("span").text.strip()
-                        stat_name = stats_titles[index]
-                        if stat == "":
-                            stat = "-1"
-                        agents_dict[stat_name] = stat
-                    elif class_name == "mod-a mod-kmax":
-                        stat = td.find("a").text.strip()
-                        stat_name = stats_titles[index]
-                        if stat == "":
-                            stat = "-1"
-                        agents_dict[stat_name] = stat
-                
-            url = f"{url}&min_rounds=0&agent="
             for agent in all_agents:
-                print(agent)
-                page = requests.get(f"{url}{agent}")
+                page = requests.get(f"{url}&min_rounds=0&agent={agent}")
+
                 soup = BeautifulSoup(page.content, "html.parser")
+                single_agent_players = set()
+                players_agents = {}
                 stats_trs = soup.find_all("tr")[1:]
+
+                if len(stats_trs) == 1:
+                    continue
+
                 for tr in stats_trs:
                     all_tds = tr.find_all("td")
                     filtered_tds = [td for td in all_tds if isinstance(td, Tag)]
@@ -195,17 +150,28 @@ for tournament_name, stages in filtered_urls.items():
                         if class_name == "mod-player mod-a":
                             player_info = td.find("div").find_all("div")
                             player, team = player_info[0].text, player_info[1].text
-                            if player not in players_with_one_agent_played:
-                                team_dict = match_type_dict.setdefault(team, {})
-                                player_dict = team_dict.setdefault(player, {})
-                            else:
-                                break
+                            team_dict = match_type_dict.setdefault(team, {})
+                            player_dict = team_dict.setdefault(player, {})
                         elif class_name == "mod-agents":
-                            img = td.find("div").find("img")
-                            src = img.get("src")
-                            match = re.search(pattern, src)
-                            agent = match.group(1)
-                            agents_dict = player_dict.setdefault(agent, {})
+                            imgs = td.find("div").find_all("img")
+                            agents = ""
+                            player_agents_dict = players_agents.setdefault(player, set())
+                            global_players_agents_dict = global_players_agents.setdefault(player, set())
+                            if agent == "all" and len(players_agents[player]) > 1:
+                                agents = ", ".join(players_agents[player]).strip(", ")
+                            elif agent == "all" and len(players_agents[player]) == 1:
+                                break
+                            elif stage_name == "All" and match_type_name == "All" and agent == "all":
+                                agents = ", ".join(global_players_agents[player]).strip(", ")
+                            else:
+                                for img in imgs:
+                                    src = img.get("src")
+                                    match = re.search(pattern, src)
+                                    agent_name = match.group(1)
+                                    player_agents_dict.add(agent_name)
+                                    agents += f"{agent}, "
+                                agents = agents.strip(", ")
+                            agents_dict = player_dict.setdefault(agents, {})
                         elif class_name == "mod-rnd" or class_name == "mod-cl" or class_name == "":
                             stat = td.text.strip()
                             stat_name = stats_titles[index]
@@ -224,13 +190,62 @@ for tournament_name, stages in filtered_urls.items():
                             if stat == "":
                                 stat = "-1"
                             agents_dict[stat_name] = stat
+                    global_players_agents[player] = global_players_agents[player] | players_agents[player]
+                # url = f"{url}&min_rounds=0&agent="
+                # for agent in all_agents:
+                #     print(agent)
+                #     page = requests.get(f"{url}{agent}")
+                #     soup = BeautifulSoup(page.content, "html.parser")
+                #     stats_trs = soup.find_all("tr")[1:]
+                #     for tr in stats_trs:
+                #         all_tds = tr.find_all("td")
+                #         filtered_tds = [td for td in all_tds if isinstance(td, Tag)]
+                #         for index, td in enumerate(filtered_tds):
+                #             td_class = td.get("class") or ""
+                #             class_name = " ".join(td_class)
+                #             if class_name == "mod-player mod-a":
+                #                 player_info = td.find("div").find_all("div")
+                #                 player, team = player_info[0].text, player_info[1].text
+                #                 if player not in players_with_one_agent_played:
+                #                     team_dict = match_type_dict.setdefault(team, {})
+                #                     player_dict = team_dict.setdefault(player, {})
+                #                 else:
+                #                     break
+                #             elif class_name == "mod-agents":
+                #                 img = td.find("div").find("img")
+                #                 src = img.get("src")
+                #                 match = re.search(pattern, src)
+                #                 agent = match.group(1)
+                #                 agents_dict = player_dict.setdefault(agent, {})
+                #             elif class_name == "mod-rnd" or class_name == "mod-cl" or class_name == "":
+                #                 stat = td.text.strip()
+                #                 stat_name = stats_titles[index]
+                #                 if stat == "":
+                #                     stat = "-1"
+                #                 agents_dict[stat_name] = stat
+                #             elif class_name == "mod-color-sq mod-acs" or class_name ==  "mod-color-sq":
+                #                 stat = td.find("div").find("span").text.strip()
+                #                 stat_name = stats_titles[index]
+                #                 if stat == "":
+                #                     stat = "-1"
+                #                 agents_dict[stat_name] = stat
+                #             elif class_name == "mod-a mod-kmax":
+                #                 stat = td.find("a").text.strip()
+                #                 stat_name = stats_titles[index]
+                #                 if stat == "":
+                #                     stat = "-1"
+                #                 agents_dict[stat_name] = stat
             break
         break
     break
 
+# print(players_stats)
+
+print(global_players_agents)
+
+
 #only on all agents. Need to get on idnividual agents
 
-print(players_stats)
                     
 
 
