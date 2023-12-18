23ab6a4a44d2a8154b83ed0b389b822fa388f091
Ryan Luong <ryanluong83@gmail.com>
2023-12-18 13:41:15 -0800

    Created a function to retrieve hte foreign key based on a value so I can reuse it

---
+++ retrieve/retrieve.py
@@ -0,0 +1,10 @@
+from Connect.execute_query import execute_query
+
+def retrieve_foreign_key(curr, foreign_key, table, column_name, column_value):
+    sql = f"SELECT {foreign_key} FROM {table} WHERE {column_name} = '{column_value}'"
+    execute_query(curr, sql)
+    result = curr.fetchone()
+    if result:
+        print(result[0])
+    else:
+        print("Doesnt exist")
