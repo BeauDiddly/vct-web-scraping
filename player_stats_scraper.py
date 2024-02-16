a55003abe165808d10db9a4fefb2945a7c181f42
Ryan Luong <ryanluong83@gmail.com>
2024-02-15 23:07:09 -0800

    Capitalized 'per' in 'average damage per round' to be consistent with the rest of the column names

--- player_stats_scraper.py
+++ player_stats_scraper.py
@@ -61,7 +61,7 @@ async def main():
     players_stats_df = pd.DataFrame(all_result,
                                     columns=["Tournament", "Stage", "Match Type", "Player", "Team", "Agents", "Rounds Played",
                                             "Rating", "Average Combat Score", "Kills:Deaths", "Kill, Assist, Trade, Survive %",
-                                            "Average Damage per Round", "Kills Per Round", "Assists Per Round", "First Kills Per Round",
+                                            "Average Damage Per Round", "Kills Per Round", "Assists Per Round", "First Kills Per Round",
                                             "First Deaths Per Round", "Headshot %", "Clutch Success %", "Clutches (won/played)",
                                             "Maximum Kills in a Single Map", "Kills", "Deaths", "Assists", "First Kills", "First Deaths"])
 
