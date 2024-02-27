e046a9bce027165d9990c1f0a616f759a01bc6b7
Ryan Luong <ryanluong83@gmail.com>
2024-02-27 13:37:08 -0800

    Added a function to find csv files within matches, agents, and players

--- find_csv_files/find_csv_files.py
+++ find_csv_files/find_csv_files.py
@@ -1,11 +1,11 @@
 import os
 
-def find_csv_files(directory):
+def find_csv_files(directory, year):
     csv_files = []
 
     for root, dirs, files in os.walk(directory):
         for file in files:
             if file.endswith('.csv'):
-                csv_files.append(file)
+                csv_files.append(f"vct_{year}/matches/{file}")
     return csv_files
 
