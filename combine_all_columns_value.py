d614d7cfaabf9a6daae71efd40177833153ff0e2
Ryan Luong <ryanluong83@gmail.com>
2024-02-26 18:58:41 -0800

    Added the missing match type to the dataframe related to tourmanets, stages and match types ID

--- combine_all_columns_value.py
+++ combine_all_columns_value.py
@@ -26,19 +26,23 @@ def main():
 
         teams_mapping_dfs[year] = team_mapping_df
 
-        tournaments_stages_matches_ids_df = pd.read_csv(f"vct_{year}/ids/tournaments_stages_matches_ids.csv")
+        tournaments_stages_matches_games_ids_df = pd.read_csv(f"vct_{year}/ids/tournaments_stages_matches_games_ids.csv")
         tournaments_stages_match_types_ids_df = pd.read_csv(f"vct_{year}/ids/tournaments_stages_match_types_ids.csv")
 
         all_agents = set(agents_df['Agents'])
 
-        teams_df["Team ID"] = teams_df["Team ID"].apply(lambda id: int(id) if not math.isnan(id) else 0000)
+        teams_df["Team ID"] = teams_df["Team ID"].apply(lambda id: int(id) if not math.isnan(id) else pd.NA)
         players_dfs[year] = players_df
         teams_dfs[year] = teams_df
 
-        tournaments_stages_matches_ids_df["Stage ID"] = tournaments_stages_matches_ids_df['Stage ID'].astype(str)
+        tournaments_stages_matches_games_ids_df["Stage ID"] = tournaments_stages_matches_games_ids_df['Stage ID'].astype(str)
         tournaments_stages_match_types_ids_df["Stage ID"] = tournaments_stages_match_types_ids_df['Stage ID'].astype(str)
-        merged_df = pd.merge(tournaments_stages_matches_ids_df, tournaments_stages_match_types_ids_df[["Tournament", "Stage", "Match Type", "Match Type ID"]],
-                             on=["Tournament", "Stage", "Match Type"], how="left")
+        tournaments_stages_matches_games_ids_df.loc[:, "Match Type"] = tournaments_stages_matches_games_ids_df["Match Type"].str.strip()
+        tournaments_stages_matches_games_ids_df.loc[:, "Match Type"] = tournaments_stages_matches_games_ids_df["Match Type"].str.strip()
+        merged_df = pd.merge(tournaments_stages_matches_games_ids_df,
+                            tournaments_stages_match_types_ids_df[["Tournament", "Stage", "Match Type", "Match Type ID"]],
+                            on=["Tournament", "Stage", "Match Type"],
+                            how="left")
         merged_df["Year"] = year
 
         matches_ids_dfs[year] = merged_df
@@ -46,14 +50,19 @@ def main():
         match_type_ids = matches_ids_dfs[year].pop("Match Type ID")
 
         matches_ids_dfs[year].insert(match_type_index + 1, "Match Type ID", match_type_ids)
+
         tournaments_stages_match_types_ids_df["Year"] = year
+
         tournaments_stages_match_types_ids_dfs[year] = tournaments_stages_match_types_ids_df
+
         
         agents = agents | all_agents
 
     for year in years:
         matches_ids = pd.concat([matches_ids, matches_ids_dfs[year]], ignore_index=True)
-        tournaments_stages_match_types_ids = pd.concat([tournaments_stages_match_types_ids, tournaments_stages_match_types_ids_dfs[year]], ignore_index=True)
+        tournaments_stages_match_types_ids = pd.concat([tournaments_stages_match_types_ids,
+                                                        tournaments_stages_match_types_ids_dfs[year]],
+                                                        ignore_index=True)
         players = pd.concat([players, players_dfs[year]], ignore_index=True)
         teams = pd.concat([teams, teams_dfs[year]], ignore_index=True)
         teams_mapping = pd.concat([teams_mapping, teams_mapping_dfs[year]], ignore_index=True)
@@ -66,11 +75,23 @@ def main():
     tournaments_stages_match_types_ids.drop_duplicates(inplace=True)
     teams_mapping.drop_duplicates(inplace=True)
 
+    missing_rows = matches_ids[matches_ids["Match Type ID"].isnull()]
+    missing_match_types = missing_rows[["Tournament", "Tournament ID", "Stage", "Stage ID", "Match Type", "Match Type ID", "Year"]].drop_duplicates()
+
+    tournaments_stages_match_types_ids = pd.concat([tournaments_stages_match_types_ids, missing_match_types], ignore_index=True) \
+                                        .sort_values(by=["Year", "Tournament", "Tournament ID", "Stage", "Stage ID", "Match Type"])
+    
+    mask = (tournaments_stages_match_types_ids["Stage ID"] == "all") & (tournaments_stages_match_types_ids["Match Type ID"] == "all")
+
+    tournaments_stages_match_types_ids.loc[mask, ["Stage ID", "Match Type ID"]] = pd.NA
+
+    tournaments_stages_match_types_ids["Stage ID"] = pd.to_numeric(tournaments_stages_match_types_ids["Stage ID"])
+    tournaments_stages_match_types_ids["Match Type ID"] = pd.to_numeric(tournaments_stages_match_types_ids["Match Type ID"])
 
     players.to_csv(f"all_vct/all_players_ids.csv", index=False)
     teams.to_csv(f"all_vct/all_teams_ids.csv", index=False)
     agents.to_csv(f"all_vct/all_agents.csv", index=False)
-    matches_ids.to_csv(f"all_vct/all_matches_ids.csv", index = False)
+    matches_ids.to_csv(f"all_vct/all_matches_games_ids.csv", index = False)
     tournaments_stages_match_types_ids.to_csv("all_vct/all_tournaments_stages_match_types_ids.csv", index = False)
     teams_mapping.to_csv("all_vct/all_teams_mapping.csv", index=False)
 
