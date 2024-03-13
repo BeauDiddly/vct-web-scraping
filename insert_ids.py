6b9a75b81ac3bd531b758b21ea1858102d8689bc
Ryan Luong <ryanluong83@gmail.com>
2024-03-12 20:18:30 -0700

    Updated csv_to_df to ignore 'nan' as null value and updated the insert_ids.py to use the new function

--- insert_ids.py
+++ insert_ids.py
@@ -2,23 +2,21 @@ from initialization.add_data import *
 import pandas as pd
 from Connect.connect import connect, engine
 from initialization.create_tables import create_all_tables
+from process_df.process_df import csv_to_df
 
 def main():
     conn, curr = connect()
     sql_alchemy_engine = engine()
-    all_agents = pd.read_csv("all_vct/all_agents.csv")
-    all_players_id = pd.read_csv("all_vct/all_players_ids.csv")
-    all_matches = pd.read_csv("all_vct/all_matches_games_ids.csv")
-    all_teams_id = pd.read_csv("all_vct/all_teams_ids.csv")
-    all_teams_mapping = pd.read_csv("all_vct/all_teams_mapping.csv")
-    all_tournaments_stages_match_types = pd.read_csv("all_vct/all_tournaments_stages_match_types_ids.csv")
+    all_players_id = csv_to_df("all_vct/all_players_ids.csv")
+    all_matches = csv_to_df("all_vct/all_matches_games_ids.csv")
+    all_teams_id = csv_to_df("all_vct/all_teams_ids.csv")
+    all_tournaments_stages_match_types = csv_to_df("all_vct/all_tournaments_stages_match_types_ids.csv")
     create_all_tables(curr)    
     conn.commit()
     add_tournaments(all_tournaments_stages_match_types, sql_alchemy_engine)
     add_stages(all_tournaments_stages_match_types, sql_alchemy_engine)
     add_match_types(all_tournaments_stages_match_types, sql_alchemy_engine)
     add_matches(all_matches, sql_alchemy_engine)
-    # add_games(all_matches, sql_alchemy_engine)
     add_teams(all_teams_id, sql_alchemy_engine)
     add_players(all_players_id, sql_alchemy_engine)
     curr.close()
--- process_df/process_df.py
+++ process_df/process_df.py
@@ -7,6 +7,11 @@ import asyncio
 import numpy as np
 import sys
 
+na_values = ['', '#N/A', '#N/A N/A', '#NA', '-1.#IND',
+            '-1.#QNAN', '-NaN', '-nan', '1.#IND',
+            '1.#QNAN', 'N/A', 'NULL', 'NaN',
+            'n/a', 'null']
+
 def strip_white_space(df, column_name):
     df.loc[:, column_name] = df[column_name].str.strip()
 
@@ -23,8 +28,10 @@ def convert_missing_number(df):
             df[column] = pd.to_numeric(df[column], errors="coerce").astype("Int32")
     return df
 
-# def convert_missing_duration(df, column_name):
-#     mask = df[column_name].notna
+
+def convert_column_to_str(df, column_name):
+    df[column_name] = df[column_name].astype(str)
+    return df
 
 def convert_column_to_int(df, column_name):
     df[column_name] = pd.to_numeric(df[column_name]).astype(int)
@@ -41,7 +48,7 @@ def rename_columns(df, columns_names):
     return df.rename(columns=columns_names)
 
 def csv_to_df(file):
-    return pd.read_csv(file)
+    return pd.read_csv(file, na_values = na_values, keep_default_na=False)
 
 def create_tuples(df):
     tuples = [tuple(x) for x in df.values]
@@ -150,138 +157,3 @@ async def change_reference_name_to_id(df, year):
         )
 
     return df
-
-
-# def process_eco_stats_df(df):
-#     change_reference_name_to_id(df)
-#     df["Initiated"] = df["Initiated"].apply(lambda col: check_na(col["Initiated"], "int"), axis = 1)
-
-
-# def process_kills_df(df):
-#     change_reference_name_to_id(df)
-#     df["Player Kills"] = df["Player Kills"].apply(lambda col: check_na(col["Player Kills"], "int"), axis = 1)
-#     df["Enemy Kills"] = df["Enemy Kills"].apply(lambda col: check_na(col["Enemy Kills"], "int"), axis = 1)
-#     df["Difference"] = df["Difference"].apply(lambda col: check_na(col["Difference"], "int"), axis = 1)
-
-# def process_kills_stats_df(df):
-#     change_reference_name_to_id(df)
-#     df["2k"] = df["2k"].apply(lambda col: check_na(col["2k"], "int"), axis = 1)
-#     df["3k"] = df["3k"].apply(lambda col: check_na(col["3k"], "int"), axis = 1)
-#     df["4k"] = df["4k"].apply(lambda col: check_na(col["4k"], "int"), axis = 1)
-#     df["5k"] = df["5k"].apply(lambda col: check_na(col["5k"], "int"), axis = 1)
-#     df["1v1"] = df["1v1"].apply(lambda col: check_na(col["1v1"], "int"), axis = 1)
-#     df["1v2"] = df["1v2"].apply(lambda col: check_na(col["1v2"], "int"), axis = 1)
-#     df["1v3"] = df["1v3"].apply(lambda col: check_na(col["1v3"], "int"), axis = 1)
-#     df["1v4"] = df["1v4"].apply(lambda col: check_na(col["1v4"], "int"), axis = 1)
-#     df["1v5"] =  df["1v5"].apply(lambda col: check_na(col["1v5"], "int"), axis = 1)
-
-# def process_maps_scores_df(df):
-#     change_reference_name_to_id(df)
-#     df["Team A Overtime Score"] = df["Team A Overtime Score"].apply(lambda col: check_na(col["Team A Overtime Score"], "int"), axis = 1)
-#     df["Team B Overtime Score"] = df["Team B Overtime Score"].apply(lambda col: check_na(col["Team B Overtime Score"], "int"), axis = 1)
-#     df["Duration"] = df["Duration"].apply(lambda col: check_na(col["Duration"], "interval"), axis = 1)
-
-# def process_overview_df(df, curr):
-#     change_reference_name_to_id(df)
-#     df["Rating"] = df["Rating"].apply(lambda col: check_na(col["Rating"], "float"), axis = 1)
-#     df["Average Combat Score"] = df["Average Combat Score"].apply(lambda col: check_na(col["Average Combat Score"], "int"), axis = 1)
-#     df["Kills"]  = df["Kills"].apply(lambda col: check_na(col["Kills"], "int"), axis = 1)
-#     df["Deaths"] = df["Deaths"].apply(lambda col: check_na(col["Deaths"], "int"), axis = 1)
-#     df["Assists"] = df["Assists"].apply(lambda col: check_na(col["Assists"], "int"), axis = 1)
-#     df["Kill - Deaths (KD)"] = df["Kill - Deaths (KD)"].apply(lambda col: check_na(col["Kill - Deaths (KD)"], "int"), axis = 1)
-#     df["Kill, Assist, Trade, Survive %"] = df["Kill, Assist, Trade, Survive %"].apply(lambda col: check_na(col["Kill, Assist, Trade, Survive %"], "percentage"), axis = 1)
-#     df["Average Damage per Round"] = df["Average Damage per Round"].apply(lambda col: check_na(col["Average Damage per Round"], "int"), axis = 1)
-#     df["Headshot %"] = df["Headshot %"].apply(lambda col: check_na(col["Headshot %"], "percentage"), axis = 1)
-#     df["First Kills"] = df["First Kills"].apply(lambda col: check_na(col["First Kills"], "int"), axis = 1)
-#     df["First Deaths"] = df["First Deaths"].apply(lambda col: check_na(col["First Deaths"], "int"), axis = 1)
-#     df["Kills - Deaths (FKD)"] = df["Kills - Deaths (FKD)"].apply(lambda col: check_na(col["Kills - Deaths (FKD)"], "int"), axis = 1)
-
-# def process_agents_pick_rates_df(df):
-#     change_reference_name_to_id(df)
-#     df["Pick Rate"] = df["Pick Rate"].apply(lambda col: check_na(col["Pick Rate"], "percentage"), axis = 1)
-
-# def process_maps_stats_df(df):
-#     change_reference_name_to_id(df)
-#     df["Attacker Side Win Percentage"] = df["Attacker Side Win Percentage"].apply(lambda col: float(col["Attacker Side Win Percentage"].strip("%")) / 100.0)
-#     df["Defender Side Win Percentage"] = df["Defender Side Win Percentage"].apply(lambda col: float(col["Defender Side Win Percentage"].strip("%")) / 100.0)
-
-# def process_players_stats_df(df):
-#     change_reference_name_to_id(df)
-#     df["Rating"] = df["Rating"].apply(lambda col: check_na(col["Rating"], "float"), axis = 1)
-#     df["Kill, Assist, Trade, Survive %"] = df["Kill, Assist, Trade, Survive %"].apply(lambda col: check_na(col["Kill, Assist, Trade, Survive %"], "percentage"), axis = 1)
-#     df["Average Damage per Round"] = df["Average Damage per Round"].apply(lambda col: check_na(col["Average Damage per Round"], "int"), axis = 1)
-#     df["First Kills Per Round"] = df["First Kills Per Round"].apply()
-#     df["First Deaths Per Round"] = df["First Deaths Per Round"].apply()
-#     df["Headshot %"] = df["Headshot %"].apply(lambda col: check_na(col["Headshot %"], "percentage"), axis = 1)
-#     df["Clutch Success %"] = df["Clutch Success %"].apply(lambda col: check_na(col["Clutch Success %"], "percentage"), axis = 1)
-#     df["Clutches (won/played)"] = df["Clutches (won/played)"].apply(lambda col: check_na(col["Clutches (won/played)"]), axis = 1)
-
-
-# def process_df(file_names, year):
-#     files_df = {}
-#     for file_name in file_names:
-#         files_df[file_names] = pd.read_csv(file_names)
-
-#     for file_name, df in files_df.items():
-#         if file_name in ["draft_phrase.csv", "eco_rounds.csv", "eco_stats.csv", "maps_played.csv", "rounds_kills.csv", "scores.csv", "teams_picked_agents.csv"]:
-#             change_reference_name_to_id(df)
-#         elif file_name == "kills.csv":
-#             process_kills_df(df)
-#         elif file_name == "kills_stats.csv":
-#             process_kills_stats_df(df)
-
-#         elif file_name == "maps_scores.csv":
-#             process_maps_scores_df(df)
-
-#         elif file_name == "overview.csv":
-#             process_overview_df(df)
-        
-#         elif file_name == "agents_pick_rates.csv":
-#             process_agents_pick_rates_df(df)
-
-#         elif file_name == "maps_stats.csv":
-#             process_maps_stats_df(df)
-
-#         elif file_name == "players_stats.csv":
-#             process_players_stats_df(df)
-#     return files_df
-
-# ('ZMM.B',)
-# teams
-# ('ASTRO',)
-# teams
-# ('FTZ.R',)
-# teams
-# ('KRAKEN',) x
-# teams
-# ('STY',)
-# teams
-# ('SR.GC',)
-# teams
-# ('ZMM.S',)
-# teams
-# ('Azules',)
-# teams
-# ('Arrow',)
-# teams
-# ('SPX',)
-# teams
-# ('TAIYO',)
-# teams
-# ('ex-WC',)
-# teams
-# ('MXBSS',)
-# teams
-# ('VCTRY',)
-# teams
-# ('kom4ka',)
-# teams
-# ('ODIUM',)
-# teams
-# ('Vexed',)
-# teams
-# ('Fre',)
-# teams
-# ('GLD.GC',)
-# teams
-# ('MANGAL',)
