3c48c45b8f19a3ca133ac1b1570c584fede01c51
Ryan Luong <ryanluong83@gmail.com>
2024-03-30 11:35:21 -0700

    Added code to drop all the tables for testing

---
+++ drop_tables.py
@@ -0,0 +1,33 @@
+from Connect.execute_query import execute_query
+from Connect.connect import connect
+
+conn, curr = connect()
+query = """
+    DROP TABLE IF EXISTS drafts CASCADE;
+    DROP TABLE IF EXISTS eco_rounds CASCADE;
+    DROP TABLE IF EXISTS eco_stats CASCADE;
+    DROP TABLE IF EXISTS kills CASCADE;
+    DROP TABLE IF EXISTS kills_stats CASCADE;
+    DROP TABLE IF EXISTS maps_played CASCADE;
+    DROP TABLE IF EXISTS maps_scores CASCADE;
+    DROP TABLE IF EXISTS overview CASCADE;
+    DROP TABLE IF EXISTS rounds_kills CASCADE;
+    DROP TABLE IF EXISTS scores CASCADE;
+    DROP TABLE IF EXISTS agents_pick_rates CASCADE;
+    DROP TABLE IF EXISTS maps_stats CASCADE;
+    DROP TABLE IF EXISTS teams_picked_agents CASCADE;
+    DROP TABLE IF EXISTS players_stats CASCADE;
+    DROP TABLE IF EXISTS stages CASCADE;
+    DROP TABLE IF EXISTS match_types CASCADE;
+    DROP TABLE IF EXISTS matches CASCADE;
+    DROP TABLE IF EXISTS games CASCADE;
+    DROP TABLE IF EXISTS maps CASCADE;
+    DROP TABLE IF EXISTS teams CASCADE;
+    DROP TABLE IF EXISTS players CASCADE;
+    DROP TABLE IF EXISTS agents CASCADE;
+    DROP TABLE IF EXISTS tournaments CASCADE;
+"""
+execute_query(curr, query)
+conn.commit()
+curr.close()
+conn.close()
