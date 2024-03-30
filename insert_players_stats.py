b4f22fc16037daff5442b2b56b8889a689c867fd
Ryan Luong <ryanluong83@gmail.com>
2024-03-30 11:34:32 -0700

    Forgot to add the insertion of players and agents stats

---
+++ insert_players_stats.py
@@ -0,0 +1,35 @@
+import os
+from Connect.connect import connect, engine
+from initialization.create_tables import create_all_tables
+import asyncio
+from initialization.add_data import *
+from find_csv_files.find_csv_files import find_csv_files
+from Connect.config import config
+
+async def main():
+    start_time = time.time()
+    now = datetime.now()
+    years = [2021, 2022, 2023]
+    conn, curr = connect()
+    sql_alchemy_engine = engine()
+    db_conn_info = config()
+
+    csv_files = [find_csv_files(f"{os.getcwd()}/vct_{year}/players_stats", "players_stats", year) for year in years]
+    print(csv_files)
+
+    await add_players_stats(csv_files[0][0], years[0], sql_alchemy_engine)
+
+
+    end_time = time.time()
+    elasped_time = end_time - start_time
+    hours, remainder = divmod(elasped_time, 3600)
+    minutes, seconds = divmod(remainder, 60)
+    print(f"Time: {int(hours)} hours, {int(minutes)} minutes, {int(seconds)} seconds")
+    # rounds_kills = pd.read_csv("matches/rounds_kills.csv")
+
+    # conn.commit()
+    # curr.close()
+    # conn.close()
+
+if __name__ == '__main__':
+    asyncio.run(main())
